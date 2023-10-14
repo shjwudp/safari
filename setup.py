8724c07a89b8b433fcd366fd1209ee8ef4ec6d52
Jianbin Chang <shjwudp@gmail.com>
2023-10-14 22:42:38 +0800

    feat(setup): transform repo into a Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="safari",
+    packages=find_packages(exclude=[]),
+    version="0.0.1",
+    license="MIT",
+    description="",
+    install_requires=[],
+)
